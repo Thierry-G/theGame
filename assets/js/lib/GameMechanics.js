879485f0687b11be43887dffbe91f7ba508c28b5
Darquanquier <darquanquier@gmail.comm>
2024-04-23 17:06:01 +0200

    setup

--- assets/js/lib/GameMechanics.js
+++ assets/js/lib/GameMechanics.js
@@ -1,3 +1,5 @@
+import GameMechanics from './GameMechanics.js';
+
 class GameMechanics {
     constructor() {
         this.squares = [];
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
 import Game from './lib/Game.js';
+
+window.onload = function () {
+    const game = new Game();
+    game.start();
+}
